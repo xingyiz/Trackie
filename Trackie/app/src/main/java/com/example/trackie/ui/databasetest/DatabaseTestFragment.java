a478e65430f7c7862ee776a27c8424e5820a6ad5
Han Xing Yi <hxingyi104@gmail.com>
2021-03-05 02:15:43 +0800

    can retrieve floorplan now

--- Trackie/app/src/main/java/com/example/trackie/ui/databasetest/DatabaseTestFragment.java
+++ Trackie/app/src/main/java/com/example/trackie/ui/databasetest/DatabaseTestFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
 import com.example.trackie.R;
 import com.example.trackie.database.OnCompleteCallback;
 import com.example.trackie.database.FirestoreHelper;
@@ -22,11 +24,14 @@ import com.example.trackie.database.MapData;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.Timestamp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class DatabaseTestFragment extends Fragment {
     private MaterialButton getButton;
@@ -34,13 +39,14 @@ public class DatabaseTestFragment extends Fragment {
     private MaterialTextView textView;
     private MaterialButton removeButton;
     private List<MapData> mapDataList;
+    private ImageView image;
 
     private Map<String, List<Integer>> data;
     private PointF location = new PointF((float) 13.0, (float) 15.0);
     private double z = 1.0;
     private String device = "Samsung Galaxy S20";
     private Timestamp timestamp = Timestamp.now();
-    private String floorplan = "test floorplan";
+    private String floorplan = "https://firebasestorage.googleapis.com/v0/b/trackie-2e28a.appspot.com/o/hello%20sutd?alt=media&token=8b0f84e5-fbdc-46cf-b60f-f779adb8eaa5";
 
     private DatabaseTestViewModel viewModel;
 
@@ -59,6 +65,7 @@ public class DatabaseTestFragment extends Fragment {
         setButton = view.findViewById(R.id.set_button);
         removeButton = view.findViewById(R.id.remove_button);
         textView = view.findViewById(R.id.database_display);
+        image = view.findViewById(R.id.image_database);
         data = new HashMap<>();
         data.put("BSSID OF WIFI AP", Arrays.asList(-82, -84, -83, 100, -86));
         data.put("ANOTHER BSSID", Arrays.asList(-83, -84, -83, -90, -85));
@@ -72,74 +79,101 @@ public class DatabaseTestFragment extends Fragment {
         getButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(getContext(), "Clicked", Toast.LENGTH_SHORT).show();
-                FirestoreHelper.GetMapData getter = new FirestoreHelper.GetMapData("HELLO WORLD");
-                getter.execute(new OnCompleteCallback() {
-                    @Override
-                    public void onSuccess() {
-                        mapDataList = getter.getResult();
-                        textView.setText(mapDataList.toString());
-                        }
-
-                    @Override
-                    public void onFailure() {
-                        Toast.makeText(getContext(), "Get Data failed.", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onError() {
-                        // error handling
-                    }
-                });
+                getData(false);
             }
         });
 
         setButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(getContext(), "Clicked", Toast.LENGTH_SHORT).show();
-                MapData mapData = new MapData("HELLO WORLD", data, location, z, device, timestamp, floorplan);
-                FirestoreHelper.SetMapData setter = new FirestoreHelper.SetMapData(mapData);
-                setter.execute(new OnCompleteCallback() {
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(getContext(), "Data set success", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onFailure() {
-                        Toast.makeText(getContext(), "Data set fail :(", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onError() {
-                        // error handling
-                    }
-                });
+                setData();
             }
         });
 
         removeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirestoreHelper.RemoveMapData remover = new FirestoreHelper.RemoveMapData(mapDataList.get(0));
-                remover.execute(new OnCompleteCallback() {
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(getContext(), "Successfully removed", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onFailure() {
-
-                    }
-
-                    @Override
-                    public void onError() {
-
-                    }
-                });
+                if (mapDataList == null) {
+                    getData(true);
+                } else if (mapDataList.size() == 0){
+                    Toast.makeText(getContext(), "Nothing to remove", Toast.LENGTH_SHORT).show();
+                } else {
+                    removeData();
+                }
             }
         });
     }
+
+    public void getData(boolean remove) {
+        FirestoreHelper.GetMapData getter = new FirestoreHelper.GetMapData("HELLO WORLD");
+        getter.execute(new OnCompleteCallback() {
+            @Override
+            public void onSuccess() {
+                if (!remove) {
+                    mapDataList = getter.getResult();
+                    textView.setText(mapDataList.toString());
+
+                    if (mapDataList.size() > 0) {
+                        StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl(mapDataList.get(0).getFloorplan());
+                        Glide.with(requireContext())
+                                .load(ref)
+                                .into(image);
+                    }
+                } else {
+                    removeData();
+                }
+            }
+
+            @Override
+            public void onFailure() {
+                Toast.makeText(getContext(), "Get Data failed.", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError() {
+                // error handling
+            }
+        });
+    }
+
+    public void setData() {
+        MapData mapData = new MapData("HELLO WORLD", data, location, z, device, timestamp, floorplan);
+        FirestoreHelper.SetMapData setter = new FirestoreHelper.SetMapData(mapData);
+        setter.execute(new OnCompleteCallback() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getContext(), "Data set success", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure() {
+                Toast.makeText(getContext(), "Data set fail :(", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError() {
+                // error handling
+            }
+        });
+    }
+
+    public void removeData() {
+            FirestoreHelper.RemoveMapData remover = new FirestoreHelper.RemoveMapData(mapDataList.get(0));
+            remover.execute(new OnCompleteCallback() {
+                @Override
+                public void onSuccess() {
+                    Toast.makeText(getContext(), "Successfully removed", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure() {
+
+                }
+
+                @Override
+                public void onError() {
+
+                }
+            });
+        }
 }
