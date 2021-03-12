2f07ded0abcd9fc5c0042f863a5491e2838ec9c3
Han Xing Yi <hxingyi104@gmail.com>
2021-03-12 11:42:41 +0800

    dk if this works help test pls

--- Trackie/app/src/main/java/com/example/trackie/ui/mapmode/RSSIUtils.java
+++ Trackie/app/src/main/java/com/example/trackie/ui/mapmode/RSSIUtils.java
@@ -1,5 +1,7 @@
 package com.example.trackie.ui.mapmode;
 
+import static java.lang.Math.pow;
+
 public class RSSIUtils {
 
    /* calculates line of sight distance using Propagation Path-Loss Model Estimate
@@ -8,10 +10,38 @@ public class RSSIUtils {
     @param    RSSI: rssi received from ScanResult
 
     */
-    public static double calculateDistance(int RSSI, int A, int n) {
-        double power = (A - RSSI)/(10*n);
+    public static double calculateDistance(int RSSI, int A) {
+        double n = 2.5;
+        double power = (double) (A - RSSI)/(10*n);
         double distance = Math.pow(10, power);
         return distance;
+    }
+
+    /**
+     * RSSI TO DISTANCE
+     * @param RSSI in dBm
+     * @param frequency
+     * @return distance
+     */
+    public static double rssiToDistance(int RSSI, int frequency) {
+        // https://electronics.stackexchange.com/questions/83354/calculate-distance-from-rssi
+
+        // taking ref dist at 1m
+        double d0 = 1.0;
+        double d = 0.1;
+        if ( RSSI > -50) {
+            d = 0.5;
+        } else if (RSSI > -60 && RSSI <= -50) {
+            d = 1.0;
+        } else if ( RSSI > -70 && RSSI <= -60) {
+            d = 1.8;
+        } else if (RSSI <= -70) {
+            d = 2.5;
+        }
+        double FSPL = 20 * Math.log10(d) + 20 * Math.log10(frequency) - 27.55;
+        double constant = 20 * Math.log10(4 * Math.PI * d0 / 0.125);
+        double n = 2.5;
+        return Math.pow(10, (FSPL - constant) * d0 / (10 * n));
     }
 
 }
