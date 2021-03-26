ba1ce2b172b238275b2a0d4fe374851d60f3bb2b
miles <victoriayong@gmail.com>
2021-03-26 16:27:32 +0800

    mOar tEsts

--- Trackie/app/src/androidTest/java/com/example/trackie/LocationsScreenTest.java
+++ Trackie/app/src/androidTest/java/com/example/trackie/LocationsScreenTest.java
@@ -1,9 +1,13 @@
 package com.example.trackie;
 
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import com.android21buttons.fragmenttestrule.FragmentTestRule;
+import com.example.trackie.ui.Prefs;
 import com.example.trackie.ui.locations.LocationsFragment;
 
 import com.example.trackie.ui.settings.SettingsFragment;
@@ -16,6 +20,7 @@ import org.junit.runners.JUnit4;
 
 
 import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.*;
@@ -23,21 +28,23 @@ import static androidx.test.espresso.assertion.ViewAssertions.*;
 
 @RunWith(AndroidJUnit4.class)
 public class LocationsScreenTest {
+
+    Context context;
+
     @Rule
     public FragmentTestRule<?, LocationsFragment> fragmentLocationsRule =
             FragmentTestRule.create(LocationsFragment.class);
 
-    @Test
-    public void addLocation() throws Exception{
-        onView(withId(R.id.upload_location_button)).perform(click()).check(matches(isDisplayed()));
-
-        //inside add locations page
-
+    @Before
+    public void Setup() {
+        context = fragmentLocationsRule.getActivity().getApplicationContext();
     }
 
     @Test
     public void setLocation() throws Exception{
         for (int i = 0; i < 5; i++) {
+            // get name of button to click
+
             onView(withId(R.id.locations_recycler_view))
                     .perform(actionOnItemAtPosition(i, click()));
 
